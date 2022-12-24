a37f346d5dac38d753180589b384466833b01d8d
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-12-24 23:45:44 +0200

    Update nuke.h

--- src/nuke.h
+++ src/nuke.h
@@ -6,6 +6,5 @@
 int nuke(const char* drv, int only_zero, int nreps, int ask_confirm);
 int confirm(const char* drv);
 void clear_drv(int fd_drv, size_t count, size_t bs, off_t seek_loc);
-void rand_drv(int fd_drv, size_t count, size_t bs, off_t seek_loc);
 
 #endif  
