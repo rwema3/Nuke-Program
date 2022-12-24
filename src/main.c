3f53feb58a925edf688b42f66aae28ecb06508c5
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-12-24 23:45:19 +0200

    Nuke Program

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
  * zeroes and random bytes */
 
 /*
- *    Copyright (C) 2020 Jithin Renji
+ *    Copyright (C) 2022 Rwema Bagirishya
  *
  *    This file is part of Nuke.
  *
@@ -13,13 +13,6 @@
  *    the Free Software Foundation, either version 3 of the License, or
  *    (at your option) any later version.
  *
- *    Nuke is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
- *
- *    You should have received a copy of the GNU General Public License
- *    along with Nuke.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include <stdio.h>
